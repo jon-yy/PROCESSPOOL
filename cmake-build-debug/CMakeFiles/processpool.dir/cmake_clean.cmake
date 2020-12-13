e73d9af4dc76ef43fba3245bba79231d2ebdbcf6
username <237889073@qq.com>
2020-12-14 00:19:42 +0800

    first commit

---
+++ cmake-build-debug/CMakeFiles/processpool.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/processpool.dir/main.cpp.o"
+  "processpool"
+  "processpool.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/processpool.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/processpool.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for processpool.
+# This may be replaced when dependencies are built.
